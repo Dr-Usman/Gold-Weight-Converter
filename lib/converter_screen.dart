46b1c6e99af1ff2957061b7296f534dbabf0c80a
Usman <dr.usman7860@gmail.com>
2026-05-13 17:55:04 +0500

    Use FocusManager to unfocus on drawer open
    Replace direct platform call to hide the keyboard with FocusManager.instance.primaryFocus?.unfocus(), removing the flutter/services import and related comments. Also move the drawer property above onDrawerChanged for clearer widget ordering. This avoids using low-level SystemChannels and cleans up unused imports.

--- lib/converter_screen.dart
+++ lib/converter_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gold_weight_converter/constants/app_colors.dart';
 import 'package:gold_weight_converter/constants/unit_enum.dart';
@@ -248,15 +247,12 @@ class _GoldConverterScreenState extends ConsumerState<GoldConverterScreen> {
           ),
         ),
       ),
+      drawer: const AppDrawer(),
       onDrawerChanged: (isOpened) {
         if (isOpened) {
-          // Unfocus the current focus node
-          FocusScope.of(context).unfocus();
-          // Directly hide the keyboard through platform channel
-          SystemChannels.textInput.invokeMethod('TextInput.hide');
+          FocusManager.instance.primaryFocus?.unfocus();
         }
       },
-      drawer: const AppDrawer(),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
